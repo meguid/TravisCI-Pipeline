6bc522ab82799c49f2b4ef5e5762810a573e5d3f
Ahmed Meguid <ameguid12@gmail.com>
2018-09-04 16:09:04 +0200

    add unit testing to app and test travis

--- travis-ci-exTests/travis_ci_exTests.swift
+++ travis-ci-exTests/travis_ci_exTests.swift
@@ -11,26 +11,51 @@ import XCTest
 
 class travis_ci_exTests: XCTestCase {
     
+    var start: Start!
+    var buttons: [UIButton]!
+    var firstButton: UIButton!
+    var secondButton: UIButton!
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        start = Start()
+        firstButton = UIButton()
+        secondButton = UIButton()
+        buttons = [firstButton, secondButton]
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        start = nil
+        firstButton = nil
+        secondButton = nil
+        buttons = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testgetIntIndex() {
+        
+        // test nil index
+        XCTAssertNotNil(start.getIntIndex(for: firstButton, at: buttons))
+        XCTAssertNotNil(start.getIntIndex(for: secondButton, at: buttons))
+        XCTAssertNil(start.getIntIndex(for: UIButton(), at: buttons))
+        
+        // test index after remove and append again
+        XCTAssertNotNil(start.getIntIndex(for: firstButton, at: buttons))
+        buttons.remove(at: 0)
+        XCTAssertNil(start.getIntIndex(for: firstButton, at: buttons))
+        buttons.insert(firstButton, at: 0)
+        XCTAssertNotNil(start.getIntIndex(for: firstButton, at: buttons))
+
+        // test gettign integer index
+        XCTAssertEqual(start.getIntIndex(for: firstButton, at: buttons), 0)
+        XCTAssertEqual(start.getIntIndex(for: secondButton, at: buttons), 1)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testdetectStatus() {
+        
+        XCTAssertEqual(start.detectStatus(index: 0), Status.invalid.rawValue)
+        XCTAssertEqual(start.detectStatus(index: 1), Status.fine.rawValue)
+
     }
     
 }
