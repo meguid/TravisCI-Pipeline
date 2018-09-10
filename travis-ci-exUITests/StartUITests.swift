5a18bd71f2e984b78cdddded4489e8afcc41deaf
Ahmed Meguid <ameguid12@gmail.com>
2018-09-10 17:02:38 +0200

    fail ui test

--- travis-ci-exUITests/StartUITests.swift
+++ travis-ci-exUITests/StartUITests.swift
@@ -27,6 +27,8 @@ class StartUITests: XCTestCase {
     }
 
     func testExample() {
+        
+        XCTAssert(false)
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
