f6731a4816da587488b0686ec53f9efc18c51fa4
Ahmed Meguid <ameguid12@gmail.com>
2018-09-10 16:57:02 +0200

    fix token

--- travis-scripts/ipa-upload.sh
+++ travis-scripts/ipa-upload.sh
@@ -14,7 +14,7 @@ RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER Uploaded: $RELEASE_DATE"
 
 HOCKEY_ID="2e7ea129f2964dfd99fed6d2b1449d5d"
-HOCKEY_TOKEN="8ae40bc6e6d09aa8e7acc7ff4d8733a4"
+HOCKEY_TOKEN="6bb70cc550964f269ca4fc74ecf6cba5"
 
 if [ ! -z "$HOCKEY_ID" ] && [ ! -z "$HOCKEY_TOKEN" ]; then
 
