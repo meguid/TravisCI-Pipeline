43c597791407e5e3ca32f2c841ed2f44bfe96dcd
Ahmed Meguid <ameguid12@gmail.com>
2018-09-10 15:32:52 +0200

    fix prov

--- travis-scripts/ipa-signing.sh
+++ travis-scripts/ipa-signing.sh
@@ -1,13 +1,13 @@
 # file: $SCRIPT_FOLDER/ipa-signing.sh
 #!/bin/sh
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-echo "This is a pull request. No deployment will be done."
-exit 0
-fi
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-echo "Testing on a branch other than master. No deployment will be done."
-exit 0
-fi
+#if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+#echo "This is a pull request. No deployment will be done."
+#exit 0
+#fi
+#if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+#echo "Testing on a branch other than master. No deployment will be done."
+#exit 0
+#fi
 
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/dist.mobileprovision"
 OUTPUTDIR="$PROJECT_DIRECTORY/build/Release-iphoneos"
@@ -15,4 +15,4 @@ OUTPUTDIR="$PROJECT_DIRECTORY/build/Release-iphoneos"
 echo "***************************"
 echo "*        Signing          *"
 echo "***************************"
-xcodebuild -exportArchive -sdk iphoneos -archivePath ArchiveFolder/travis-ci-ex.xcarchive -exportPath "$OUTPUTDIR" -exportOptionsPlist "ipa.plist"
+xcodebuild -exportArchive -archivePath ArchiveFolder/travis-ci-ex.xcarchive -exportOptionsPlist "ExportOptions.plist" -exportPath "$OUTPUTDIR" -allowProvisioningUpdates
